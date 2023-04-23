56592d8e9eddb5cd7f587a6c0d5bc5bcef4c04a1
LvaMai <102939446+mid0w0dim@users.noreply.github.com>
2023-04-23 21:11:50 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -368,17 +368,17 @@ async def on_voice_state_update(member, before, after):
     if before.channel:  # ユーザーがボイスチャンネルから退出した場合
         private_channel = private_channels.get(before.channel.id)
         if private_channel:
-        　　# メンバーがボットでない場合にのみ、権限をリセットする
-        　　if not member.bot:
-           　　 await private_channel.set_permissions(member, read_messages=None)
+            # メンバーがボットでない場合にのみ、権限をリセットする
+            if not member.bot:
+                await private_channel.set_permissions(member, read_messages=None)
 
-           # ボイスチャンネルに誰もいない場合は、チャットをクリアする
-           if len(before.channel.members) == 0:
-               async for message in private_channel.history(limit=50):
-                   try:
-                       await message.delete()
-                   except discord.errors.NotFound:
-                       pass  # メッセージが既に削除されている場合は無視する
+            # ボイスチャンネルに誰もいない場合は、チャットをクリアする
+            if len(before.channel.members) == 0:
+                async for message in private_channel.history(limit=50):
+                    try:
+                        await message.delete()
+                    except discord.errors.NotFound:
+                        pass  # メッセージが既に削除されている場合は無視する
 
 # ヘルプコマンド
 @tree.command(name="help", description="このボットの使い方を表示します。")
